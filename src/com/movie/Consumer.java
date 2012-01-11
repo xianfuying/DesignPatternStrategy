7e7c3e6f49a31d7bb3dec46233a212ba9a1a5772
fu ying <xianfuying@gmail.com>
2012-01-11 22:21:00 +0800

    add consumer class to use cinema

---
+++ src/com/movie/Consumer.java
@@ -0,0 +1,16 @@
+package com.movie;
+
+public class Consumer {
+    public static void main(String[] args) {
+        Strategy memberStrategy = new MemberStrategy();
+        CMBCStrategy cmbcStrategy = new CMBCStrategy();
+
+        Cinema cinema = new Cinema();
+        cinema.support(memberStrategy);
+        cinema.support(cmbcStrategy);
+
+        Payment payment = cinema.getPayment(5);
+
+        System.out.println(payment);
+    }
+}
